b05212ae33a909920f8e599283e6a2b48979937d
Souyahibou <souaybou.k@live.fr>
2017-10-22 18:31:21 +0200

    Fin de modif pour le projet morpion reste (nomdesjoueur, choix du signe, debut aléatoire, verrouillage cell ecrite...)

--- The_Hacking_Project/S3_Ruby_intermédiaire/J4_tic_tac_toe/Projet_tic-tac-toe_P2S3_V2.rb
+++ The_Hacking_Project/S3_Ruby_intermédiaire/J4_tic_tac_toe/Projet_tic-tac-toe_P2S3_V2.rb
@@ -32,20 +32,23 @@ class Board
 
 
 
-      def display_of_array_advanced
-          puts "  0 1 2"
-          puts "  _ _ _\n0|#{array_of_game[0][0].how_is_status}|#{array_of_game[0][1].how_is_status}|#{array_of_game[0][2].how_is_status}|"
-          puts "  _ _ _\n1|#{array_of_game[1][0].how_is_status}|#{array_of_game[1][1].how_is_status}|#{array_of_game[1][2].how_is_status}|"
-          puts "  _ _ _\n2|#{array_of_game[2][0].how_is_status}|#{array_of_game[2][1].how_is_status}|#{array_of_game[2][2].how_is_status}|"
-          puts "  _ _ _"
+      def display_of_array_advanced                                               #permet l'affichage du plateau de jeu
+          puts "  0 1 2 ".rjust(100)
+          puts "  _ _ _ ".rjust(100)
+          puts "0|#{array_of_game[0][0].how_is_status}|#{array_of_game[0][1].how_is_status}|#{array_of_game[0][2].how_is_status}|".rjust(100)
+          puts "  _ _ _ ".rjust(100)
+          puts "1|#{array_of_game[1][0].how_is_status}|#{array_of_game[1][1].how_is_status}|#{array_of_game[1][2].how_is_status}|".rjust(100)
+          puts "  _ _ _ ".rjust(100)
+          puts "2|#{array_of_game[2][0].how_is_status}|#{array_of_game[2][1].how_is_status}|#{array_of_game[2][2].how_is_status}|".rjust(100)
+          puts "  _ _ _ ".rjust(100)
       end
 
 
-      def action(horizontal, vertical, joueur_tag)
+      def action(horizontal, vertical, joueur_tag)                                #permet de cocher une case
           @array_of_game[horizontal][vertical].how_is_status = joueur_tag
       end
 
-      def is_end(the_player)
+      def is_end(the_player)                                                      # les régle de jeu pour gagner la manche
           res = 1
           signes = ["X", "O"]
           the_player.player_status = "win"
@@ -91,13 +94,13 @@ end
 
 
 
-class Game
+class Game  #demmare et finalise le jeu
       # c'est la partie. Elle initialise tout et lance le jeu
       attr_accessor :gamer_one,   :gamer_two,   :tab_morpion,   :tour,   :sign_win
 
       def initialize
           @tab_morpion = [[]]
-          @gamer_one = Player.new("Gamer_one","X")             #dans un 2nd temps demander la siigne et le nol du gamer
+          @gamer_one = Player.new("Gamer_one","X")     #dans un 2nd temps demander la siigne et le nol du gamer
           @gamer_two = Player.new("Gamer_two","O")
           @tab_morpion = Board.new
           @tour = 0;
@@ -105,39 +108,45 @@ class Game
       end
 
 
-      def go
-          puts "Bienvenue"                                    #randam qui commence
-          puts "joueur1 and sign"
-          puts "joueur2 and sign"
-
-
-          turn                           #gamer_first_tour premier joueur à jouer
+      def go                                        #lance le jeu affiche le message de bienvenue
+          puts "Bienvenue il n'y aura pas de random c'est le joueur 1 qui commence"
+          puts "Le nom du joueur 1 est Gamer_one, son signe est 'X'"
+          puts "Le nom du joueur 2 est Gamer_two, son signe est 'O'"
+          turn  (gamer_one)                         #gamer_first_tour premier joueur à jouer
       end
 
 
-      def turn (gamer_tour = nil)
+      def turn (gamer_tour = nil)                  # gère le changement de tour ainsi que le jeu
           if gamer_tour == nil then if tour % 2 == 0 then gamer_tour = gamer_one else gamer_tour = gamer_two end elsif gamer_tour == gamer_two then chg = gamer_two;  @gamer_two = gamer_one; @gamer_one =chg;   end
           @tour += 1
           tab_morpion.display_of_array_advanced
           gamer_tour.player_info
 
-          puts "quel case vous jouer\nligne :"                        #personnaliser avec nom des joueur
-          pos_v = gets.strip.to_i
-          puts "coll :"                        #personnaliser avec nom des joueur
-          pos_h = gets.strip.to_i
+          puts "quel case vous jouer (ex:LC, 00, 01, 02, 10, 11, 12...22,)\nLigne : Coll :"                        #personnaliser avec nom des joueur
+          cell = gets.strip.to_s
+          pos_v = cell[0].to_i
+          pos_h = cell[1].to_i
+
+
+          # tab_morpion.action(0, 0, gamer_tour.player_tag.to_s)
+          # tab_morpion.action(0, 1, gamer_tour.player_tag.to_s)
+          # tab_morpion.action(0, 2, gamer_tour.player_tag.to_s)
 
           tab_morpion.action(pos_v, pos_h, gamer_tour.player_tag.to_s)
-          if tab_morpion.is_end(gamer_tour) then fin else self.turn end
+          if tab_morpion.is_end(gamer_tour) then fin(gamer_tour) else self.turn end
       end
 
-      def fin
-          puts gamer_one.player_status
-          puts gamer_two.player_status
+      def fin(winner) #fin de partie affiches les stats
 
-          puts "messages de fin"                        #personnaliser avec nom des joueur
+          puts "\n\n\n"
+          if gamer_one.player_status == "win" then  gamer_two.player_status = "loose"   else  gamer_one.player_status = "loose" end
+
+          gamer_one.player_info
+          gamer_two.player_info
+
           tab_morpion.display_of_array_advanced
       end
 
 end
 
-puts Game.new.go
+jeu = Game.new.go

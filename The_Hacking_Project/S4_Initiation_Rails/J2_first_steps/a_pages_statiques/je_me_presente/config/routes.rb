3ae49609a97637f441fca4ba640ac1399022565d
Souyahibou <souaybou.k@live.fr>
2017-10-25 19:56:18 +0200

    commit du projet Validant 2

---
+++ The_Hacking_Project/S4_Initiation_Rails/J2_first_steps/a_pages_statiques/je_me_presente/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get '/contact', to: 'static_pages#contact'
+
+  get '/about', to: 'static_pages#about'
+
+  root 'static_pages#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
